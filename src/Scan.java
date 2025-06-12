5c45d355a5c5f4ab103b4c189a4e918f2a589d75
VitorInacioBorges <vitorinacioborges@email.com>
2025-06-12 09:50:12 -0300

    refractor altered Scan.java

--- src/Scan.java
+++ src/Scan.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+import java.util.Locale;
+
 // Using a class named "Scanner" to import the scanner method.
 public class Scan { // Giving our main class the "Scan" Name
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); 
+        scanner.useLocale(Locale.US); // Using the locale Us to read the decimal numbers correctly (Ex: 18"."67).
         /* Scanner object created and giving It 
         the following charateristics Scanner 
         class with the System.in method. */
@@ -14,14 +17,15 @@ public class Scan { // Giving our main class the "Scan" Name
 
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
-        /* System.out.print("Enter a decimal number: ");
-        double decimal = scanner.nextDouble(); */
+        scanner.nextLine();
+        System.out.print("Enter a decimal number: ");
+        double decimal = scanner.nextDouble(); 
         System.out.print("- America is bigger than Brasil -, is this true or false: ");
         boolean a = scanner.nextBoolean();
 
         System.out.println("\nHello " + name);
         System.out.println("You're " + age + " years old!");
-        /*System.out.println("The decimal number is: " + decimal);*/
+        System.out.println("The decimal number is: " + decimal);
         if(a) System.out.println("America IS bigger than Brasil!\n");
         else System.out.println("America is NOT bigger than Brasil!\n");
         scanner.close(); 
