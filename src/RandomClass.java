f5dfa2c49c54b8039572133d25ab03933444e26b
VitorInacioBorges <vitorinacioborges@email.com>
2025-06-12 11:43:10 -0300

    refractor altered Random.java

--- src/RandomClass.java
+++ src/RandomClass.java
@@ -2,8 +2,8 @@ import java.util.Random;
 public class RandomClass {
     public static void main(String[] args) {
         Random random = new Random();
-        int number = random.nextInt(1, 7);
-        System.out.println(number);
+        int numberInt = random.nextInt(1, 7);
+        System.out.println(numberInt);
         /* 
         The Random class generates a number between +2 billion and 
         - 2 billion. To set the boundaries of the generation we use
@@ -12,5 +12,12 @@ public class RandomClass {
         random.nextInt(1, 101);
         P.S. THE FIRST NUMBER IS INCLUSIVE AND THE LAST IS EXCLUSIVE!!!
          */
+        
+        double numberDouble = random.nextDouble();
+        System.out.println(numberDouble);
+        // Without any boundaries It generates a decimal number between 0 and 1
+
+        boolean trueOrFalse = random.nextBoolean();
+        System.out.println(trueOrFalse);
     }
 }
