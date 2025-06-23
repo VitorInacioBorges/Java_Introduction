a7dad40fe610a63ce94b58dd9878a421fb86dad3
VitorInacioBorges <vitorinacioborges@email.com>
2025-06-23 12:04:52 -0300

    reafractor created StringMethod.java

---
+++ src/StringMethod.java
@@ -0,0 +1,19 @@
+public class StringMethod {
+    public static void main(String[] args) {
+        String name = "Vitor Inacio";
+        char letter = name.charAt(0);
+        int nameLength = name.length();
+        int lastIndex = name.lastIndexOf("o", nameLength);
+        name = name.trim(); // vitorinacio
+        name = name.toLowerCase(); // vitor inacio
+        name = name.toUpperCase(); // VITOR INACIO
+        name = name.replace("o", "x"); // Vitxr Inacix
+
+        if(name.isEmpty()) System.out.println("name is empty");
+        else System.out.println("name is NOT empty");
+
+        System.out.println(nameLength); // 12
+        System.out.println(letter); // V
+        System.out.println(lastIndex); // 11
+    }
+}
