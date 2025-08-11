1442603dfefa8826bcb7567e3d6bc1a87b450e83
VitorInacioBorges <vitorinacioborges@email.com>
2025-08-11 10:33:16 -0300

    refractor added VariableScope.java

--- src/theory/VariableScope.java
+++ src/theory/VariableScope.java
@@ -1,7 +1,16 @@
 package theory;
-
+// Variable Scope: where a variable can be accessed
+// 2 types: LOCAL and CLASS
+// CLASS variables override LOCAL variables
 public class VariableScope {
+    static int x = 3; // CLASS variable: recognized through all the code
     public static void main(String[] args) {
-
+        int x = 1; // LOCAL variable: recognized locally
+        System.out.println(x);
+        doSomething(); // calling a doSomething method.
+    }
+    static void doSomething(){
+        int x = 2; // LOCAL variable
+        System.out.println(x);
     }
 }
