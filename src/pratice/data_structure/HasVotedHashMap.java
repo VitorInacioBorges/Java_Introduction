5cfba065b48840b4da3d2d7b5ab73948fb251f47
VitorInacioBorges <vitorinacioborges@gmail.com>
2025-08-14 14:21:30 -0300

    refractor added HasVotedHashMap.java

---
+++ src/pratice/data_structure/HasVotedHashMap.java
@@ -0,0 +1,22 @@
+package pratice.data_structure;
+
+import java.util.HashMap;
+
+public class HasVotedHashMap {
+    private static HashMap<String, Boolean> voted = new HashMap<>();
+
+    private static void checkVoter(String person){
+        if(voted.containsKey(person)){
+            System.out.println("voted already! kick them out!");
+        } else {
+            voted.put(person, true);
+            System.out.println("voted not found! let them vote!");
+        }
+    }
+
+    public static void main(String[] args){
+        checkVoter("tom"); // didn't vote
+        checkVoter("victor"); // didn't vote
+        checkVoter("victor"); // voted
+    }
+}
